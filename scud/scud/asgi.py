831f27b574aa459cbb5c0ae1fb465d219a7ba4d7
KirillGorilo <kirill.007.228.74@gmail.com>
2024-03-27 15:31:04 +0300

    initial commit

---
+++ scud/scud/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for scud project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scud.settings')
+
+application = get_asgi_application()
