ab792ecc924c155b46a60b3ebd37c0ed850dbead
KirillGorilo <kirill.007.228.74@gmail.com>
2024-03-30 16:51:45 +0300

    test qr

--- scud/check/urls.py
+++ scud/check/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('generate/', views.GenerateQRCode.as_view(), name="generate"),
 ]
--- scud/check/views.py
+++ scud/check/views.py
@@ -1,7 +1,37 @@
 from django.http import HttpResponse
+from django.views import View
 from django.shortcuts import render
+import qrcode
+import io
 
 
 def index(request):
-    return HttpResponse("hello")
+    return render(request, 'check/qrcode.html')
     
+
+class GenerateQRCode(View):
+    def get(self, request, *args): 
+        data = 'https://google.com'
+        
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(data)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+
+
+        response = HttpResponse(content_type='image/png')
+        img.save(response, 'PNG')
+
+        return response
+
+
+
+
+
+
